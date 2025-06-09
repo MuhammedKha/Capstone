890fff90d4d4df21d77cd5fade0e6593b600909a
MuhammedKha <muhammadkhan163570@gmail.com>
2025-06-09 17:18:24 +1000

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -4,6 +4,9 @@ define('DB_USER', getenv('DB_USER'));
 define('DB_PASS', getenv('DB_PASS'));
 define('DB_NAME', getenv('DB_NAME'));
 
+// ✅ Set PHP timezone to Melbourne before any time functions are used
+date_default_timezone_set('Australia/Melbourne');
+
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
 // Check connection
